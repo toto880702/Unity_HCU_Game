2c8e1fc0a573bf97a0a0872b5a6f57223933323e
toto880702 <58834372+toto880702@users.noreply.github.com>
2019-12-13 15:05:29 +0800

    子彈飛

--- Unity_1213_2/Assets/Script/player.cs
+++ Unity_1213_2/Assets/Script/player.cs
@@ -11,7 +11,60 @@ public float speed = 1.5f;
 [Header("血量"), Range(100,1000)]
 public float hp = 100;
 
-public Animator ani;
-public Rigidbody2D r2d;
+[Header("子彈物件")]
+public GameObject bullte;
+[Header("發射音效")]
+public AudioClip soundFire;
+
+public Transform point;
+
+private Animator ani;
+private Rigidbody2D r2d;
+private AudioSource aud;
 #endregion
+
+
+public float speedBullet = 1000;
+
+private void Start()
+{
+  ani = GetComponent<Animator>();
+  r2d = GetComponent<Rigidbody2D>();
+  aud = GetComponent<AudioSource>();
 }
+//一秒執行50次
+private void FixedUpdate()
+
+{
+    Move();
+
+}
+
+private void Update()
+{
+    Fire();
+}
+
+private void Move()
+{
+    float h = Input.GetAxisRaw("Horizontal"); //左A-1 右D1 沒按0
+   r2d.AddForce(new Vector2(speed*h,0));//剛體.推力(二為向量(左右.上下))
+   if (h !=0) transform.localScale = new Vector3(h,1,1);
+
+}
+
+private void Fire()
+{
+   if (Input.GetKeyDown(KeyCode.Mouse0))
+   {
+       aud.PlayOneShot(soundFire);
+       GameObject temp = Instantiate(bullte, point.position, Quaternion.identity);
+       temp.GetComponent<Rigidbody2D>().AddForce(new Vector2(speedBullet * transform.localScale.x, 80));
+   }
+
+}
+
+
+}
+
+

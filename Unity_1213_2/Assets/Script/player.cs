f19edc08781f3f33fa14140ade937ab1211290af
toto880702 <58834372+toto880702@users.noreply.github.com>
2019-12-13 12:12:20 +0800

    HCU_2D_Game

---
+++ Unity_1213_2/Assets/Script/player.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class player : MonoBehaviour 
+{
+	
+#region 欄位
+[Header("移動速度"), Range(0,2000)]
+public float speed = 1.5f;
+
+[Header("血量"), Range(100,1000)]
+public float hp = 100;
+
+public Animator ani;
+public Rigidbody2D r2d;
+#endregion
+}
